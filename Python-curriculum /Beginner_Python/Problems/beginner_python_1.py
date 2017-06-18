1727e33ba8205ebe987d6a6382d64f99c087d0c4
DianeDeeDee <dianephy@gmail.com>
2017-06-18 19:19:09 -0400

    Improve2

--- Python-curriculum /Beginner_Python/Problems/beginner_python_1.py
+++ Python-curriculum /Beginner_Python/Problems/beginner_python_1.py
@@ -1,44 +1,42 @@
 #Romove import statements if they are not used: they slow down the code
 #import random
-#import sys
-#import os
+import sys
+import os
 
 
 def factorial(n):
 
-    """factorial() is the function that will calculate the
+    """
+    factorial() is the function that will calculate the
     the factorial of a non-negative integer n. You can also
     the math package's factorial() but let's play a little bit with tools :)
     params:
-
-:n: any integer value.
-
-returns:
-
-:res: an integer, the factorial of n.
-
+      :n: any integer value.
+      returns:
+      :res: an integer, the factorial of n.
     """
+
     res = 1
     for i in range(1, n+1):
         res=res*i
     return res
 
-#print(factorial(3),"=3!")
-#print(factorial(5),"=5!")
-#print("\n\n")
-
 def is_prime(number):
-    """is_prime() is the the function that tests the primality
+
+    """
+    is_prime() is the the function that tests the primality
     of the input number will check if number is prime.
     If it's prime, boolean is_it_prime is True, otherwise it' is False.
     In number theory, Wilson's theorem states that:
-     a natural number > 1 is a prime number
-    if and only if ( number − 1 ) !   ≡   − 1 ( mod number )
+      a natural number > 1 is a prime number
+      if and only if ( number − 1 ) !   ≡   − 1 ( mod number )
     Let's define:
       fact = ( number − 1 ) ! + 1
       mod = fact % number
-        """
-
+    """
+    print("What is your number?")
+    number = sys.stdin.readline()
+    number = int(number)
     fact= (factorial(number - 1) + 1)
     mod = fact % number
     if (mod == 0):
@@ -48,8 +46,6 @@ def is_prime(number):
 
     return is_it_prime
 
-#print("factorial(b-1)=",factorial(-1),"; factorial(b-1)+1=",factorial(5-1)+1," "
- #      ";fac%b=",(factorial(5-1)+1)%5)
 print("\n\n")
 print("- Come on Magic Ball give me the answer! Is %s a prime? "%"29")
 print("-",is_prime(29),"!")
--- Python-curriculum /Beginner_Python/Problems/beginner_python_2.py
+++ Python-curriculum /Beginner_Python/Problems/beginner_python_2.py
@@ -1,25 +1,23 @@
-#import beginner_python_1
+import sys
 import beginner_python_1 as bp1
 def return_primes(number):
-    """return_primes() is the the function that the function that returns the list of all primes smaller than number.
-       list_of_primes is the list of all the prime numbers whose values are lower than number
-        """
+
+    """
+    return_primes() is the the function that the function that returns the list of all primes
+    smaller than number.
+    list_of_primes is the list of all the prime numbers whose values are lower than number
+    """
     list_of_primes = []
-    for i in range(1,1000000):
+    for i in range(1,number):
+        if ((bp1.is_prime(i))):
+            list_of_primes.append(i)
+        else:
+            continue
 
-        while i < number:
-            #print("i= ", i)
-            i += 1
-            #if ((bp1.factorial(i - 1) + 1)% i == 0):
-            if ((bp1.is_prime(i))):
-                list_of_primes.append(i)
-                #print("list_of_primes=",list_of_primes)
-            else:
-                continue
-
-        return list_of_primes
+    return list_of_primes
 
 print("\n\n")
+
 print("The list of all primes smaller than %s "%"29" " is:\n            "
                                                 "list_of_primes=",return_primes(29))
 
