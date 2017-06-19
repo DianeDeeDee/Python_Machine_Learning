7b8d0230858bd84d44fc93c6e23266e02cab7327
DianeDeeDee <dianephy@gmail.com>
2017-06-19 12:04:24 -0400

    Use of __name__ == '__main__'

--- Python-curriculum /Beginner_Python/Problems/beginner_python_1.py
+++ Python-curriculum /Beginner_Python/Problems/beginner_python_1.py
@@ -3,6 +3,15 @@
 import sys
 import os
 
+def function_1(number):
+    return number
+
+print("What is your number?")
+number = sys.stdin.readline()
+number = int(number)
+
+if __name__ == '__main__':
+	print('The number is ' + str(number))
 
 def factorial(n):
 
@@ -34,9 +43,9 @@ def is_prime(number):
       fact = ( number − 1 ) ! + 1
       mod = fact % number
     """
-    print("What is your number?")
-    number = sys.stdin.readline()
-    number = int(number)
+    #print("What is your number?")
+    #number = sys.stdin.readline()
+    #number = int(number)
     fact= (factorial(number - 1) + 1)
     mod = fact % number
     if (mod == 0):
@@ -47,13 +56,9 @@ def is_prime(number):
     return is_it_prime
 
 print("\n\n")
-print("- Come on Magic Ball give me the answer! Is %s a prime? "%"29")
-print("-",is_prime(29),"!")
+
+print("- Come on Magic Ball give me the answer! Is %s a prime? "%number)
+print("-",is_prime(number),"!")
 print("- How cool it's working! Let's try another number...")
 
 print("\n\n")
-
-print("We can improve the code so it will check all numbers in range, and not "
-      "just one specific number.")
-print("Then, we can using an output.txt where all prime number "
-      "are saved automatically. ;)")
--- Python-curriculum /Beginner_Python/Problems/beginner_python_2.py
+++ Python-curriculum /Beginner_Python/Problems/beginner_python_2.py
@@ -1,5 +1,17 @@
 import sys
 import beginner_python_1 as bp1
+#import function_1 as f_1
+
+def function_1(number):
+    return number
+
+print("What is your number again?")
+number = sys.stdin.readline()
+number = int(number)
+
+if __name__ == '__main__':
+	print('The number is ' + str(number))
+
 def return_primes(number):
 
     """
@@ -8,6 +20,7 @@ def return_primes(number):
     list_of_primes is the list of all the prime numbers whose values are lower than number
     """
     list_of_primes = []
+   # bp1.f_1(number)
     for i in range(1,number):
         if ((bp1.is_prime(i))):
             list_of_primes.append(i)
@@ -18,8 +31,8 @@ def return_primes(number):
 
 print("\n\n")
 
-print("The list of all primes smaller than %s "%"29" " is:\n            "
-                                                "list_of_primes=",return_primes(29))
+print("The list of all primes smaller than %s" %number,"is:")
+print(" list_of_primes=",return_primes(number))
 
 
 print("\n\n")
